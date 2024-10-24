76953315f05ceca5a09bcc86d7f23a157486f651
matteoltz <matteo.lutz06@outlook.de>
2024-10-24 14:52:31 +0200

    began implementing master functionality for reactions

--- app/routes/reaction/route.tsx
+++ app/routes/reaction/route.tsx
@@ -2,19 +2,34 @@ import { json, useLoaderData, useRevalidator } from '@remix-run/react';
 import { useEffect } from 'react';
 import { useEventSource } from 'remix-utils/sse/react';
 import { dbReactionTimes } from '~/services/db.server';
+import { ReactionTimeEntry } from '~/types/db';
 import { ReactionServerClientMessage } from '~/types/sse';
 
 export const loader = async () => {
-  return json(dbReactionTimes.chain().find().simplesort('time').data());
+  if (process.env.EVO_MASTER_HOST) {
+    const data = await fetch(
+      `http://${process.env.EVO_MASTER_HOST}/api/reaction`
+    ).then((r) => r.json());
+
+    return json({
+      reactions: data as Array<ReactionTimeEntry>,
+      sseUrl: `http://${process.env.EVO_MASTER_HOST}/sse/reaction`
+    });
+  }
+
+  return json({
+    reactions: dbReactionTimes.chain().find().simplesort('time').data(),
+    sseUrl: '/sse/reaction'
+  });
 };
 
 const Reaction = () => {
-  const data = useLoaderData<typeof loader>();
+  const { reactions, sseUrl } = useLoaderData<typeof loader>();
 
   const { revalidate } = useRevalidator();
 
   const serverMessage = useEventSource(
-    '/sse/reaction'
+    sseUrl
   ) as ReactionServerClientMessage | null;
 
   useEffect(() => {
@@ -36,7 +51,7 @@ const Reaction = () => {
   return (
     <div className="w-screen h-screen">
       <div className="blueprint size-full bg-evo-orange">
-        Hello Test {data.length}
+        Hello Test {reactions.length}
       </div>
     </div>
   );
