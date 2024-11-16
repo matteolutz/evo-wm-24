1c8bec108825b8cdcde10354d6926298a332a4f4
matteoltz <matteo.lutz06@outlook.de>
2024-11-16 20:17:08 +0100

    added test nfc tag

--- app/constants/nfcLut.ts
+++ app/constants/nfcLut.ts
@@ -1,5 +1,6 @@
 const NFC_LUT: Record<string, string> = {
-  '4b010100040804feff3200': '/reaction'
+  '4b010100040804feff3200': '/reaction',
+  '4b01010044000704d115f5230289': '/game/1'
 };
 
 export default NFC_LUT;
